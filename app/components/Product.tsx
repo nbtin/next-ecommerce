26a53df37fbea837039c57c67381ef90702a0c6b
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-10-22 21:57:35 +0700

    fix any-type Typescript error

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,11 +1,17 @@
-import Image from "next/image"
+import Image from "next/image";
 
-export default function Product({name, image, price}) {
-    return (
-        <div>
-            <Image src={image} alt={name} width={400} height={400}></Image>
-            <h1>{name}</h1>
-            <h2>{price}</h2>
-        </div>
-    )
+interface ProductProps {
+  name: string;
+  image: string; // Update the type to match the actual type of your image source
+  price: number; // Change this type to match the actual type of your price
+}
+
+export default function Product({ name, image, price }: ProductProps) {
+  return (
+    <div>
+      <Image src={image} alt={name} width={400} height={400}></Image>
+      <h1>{name}</h1>
+      <h2>{price}</h2>
+    </div>
+  );
 }
