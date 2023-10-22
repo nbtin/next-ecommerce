558e1d70a4d74e3fd98f4bdb62d7cfdf25f9e8ad
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-10-22 18:12:08 +0700

    create navigation bar

---
+++ app/components/Nav.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Session } from "next-auth";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+export default function Nav({ user }: Session) {
+  return (
+    <nav className="flex justify-between items-center py-8">
+      <h1>1Piece</h1>
+      <ul className="flex items-center gap-12">
+        {/* if the user is not signed in */}
+        {!user && (
+          <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
+            <button onClick={() => signIn()}>Sign in</button>
+          </li>
+        )}
+        {/* if the user is signed in */}
+        {user && (
+          <li>
+            <Image
+              src={user?.image as string}
+              alt={user?.name as string}
+              width={48}
+              height={48}
+              className="rounded-full"
+            ></Image>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 
 const prisma = new PrismaClient();
 
-export default NextAuth({
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -38,4 +38,6 @@ export default NextAuth({
       }
     },
   },
-});
+}
+
+export default NextAuth(authOptions);
