10cda67a6657735193ba411e1893af41d8fd10f6
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-10-29 15:54:48 +0700

    enhance the cart UI

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -2,13 +2,42 @@
 
 import Image from "next/image";
 import { useCartStore } from "@/store";
+import formatPrice from "@/util/PriceFormat";
 
 export default function Cart() {
   const cartStore = useCartStore();
-  console.log(cartStore.isOpen);
   return (
-    <div>
-      <h1>Cart</h1>
+    <div
+      onClick={() => cartStore.toggleCart()}
+      className="fixed w-full h-screen left-0 top-0 bg-black/25"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700"
+      >
+        <h1>Here's your shopping list 📃</h1>
+        {cartStore.cart.map((item) => (
+          <div key={item.id} className="flex py-4 gap-4">
+            <Image
+              className="rounded-md h-24"
+              src={item.image}
+              alt={item.name}
+              width={120}
+              height={120}
+            />
+            <div>
+              <h2>{item.name}</h2>
+              <h2>Quantity: {item.quantity}</h2>
+              <p className="text-sm">
+                {item.unit_amount && formatPrice(item.unit_amount)}
+              </p>
+            </div>
+          </div>
+        ))}
+        <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
+          Checkout
+        </button>
+      </div>
     </div>
   );
 }
